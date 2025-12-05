2ed8673a8dbafc3d3671f4e8c951b73545199b8d
Nischal <nyyscal541556@gmail.com>
2025-12-05 12:39:07 +0545

    Auth Initialization

---
+++ services/auth/dist/index.js
@@ -0,0 +1,6 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config();
+app.listen(process.env.PORT, () => {
+    console.log(`Auth service is running on htpp://localhost:${process.env.PORT}`);
+});
